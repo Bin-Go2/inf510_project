9c476537eee79b7a69cab1d3b15dbfed7e01cd85
SondersB <binzhangsam@gmail.com>
2019-12-11 15:32:02 -0800

    change the folder format

--- FlightInfo/airlines/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from . import models
-
-admin.site.register(models.Flight)
-admin.site.register(models.City)
-admin.site.register(models.User)
